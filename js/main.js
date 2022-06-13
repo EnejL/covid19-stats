4c33f936033308a2bbf1566c00bd63aa12aa543d
Enej Licina <elicina@nbeyond.nl>
2022-06-13 12:40:10 +0200

    added icon and manifest.json

---
+++ js/main.js
@@ -0,0 +1,16 @@
+async function fetchData() {
+    const res = await fetch ("https://api.sledilnik.org/api/summary");
+    const record = await res.json();
+    console.log(record);
+
+    let year = record.vaccinationSummary.year;
+    let month = record.vaccinationSummary.month;
+    let day = record.vaccinationSummary.day;
+
+    document.getElementById('date').innerHTML = day + '/' + month + '/' + year;
+    document.getElementById('tests-hag').innerHTML = record.testsTodayHAT.value;
+    document.getElementById('tests-pcr').innerHTML = record.testsToday.year;
+    document.getElementById('active-cases').innerHTML = record.casesActive.subValues.in;
+    document.getElementById('total-deaths').innerHTML = record.deceasedToDate.value;
+}
+fetchData();
